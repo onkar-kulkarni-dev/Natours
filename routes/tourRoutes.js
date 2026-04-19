ff44f8406cff0fc74b4258b60174edd9896606a6
onkar <onksgk@gmail.com>
2026-04-19 10:30:53 +0530

    created separate repo

---
+++ routes/tourRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const tourController = require("../controller/tourController");
+
+const router = express.Router();
+
+//param validation for id
+// router.param('id', tourController.checkId)
+
+router
+  .route("/")
+  .get(tourController.getAllTours)
+  // .post(tourController.checkBody, tourController.createTour);
+  .post(tourController.createTour);
+
+router
+  .route("/:id")
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
+
+module.exports = router;
