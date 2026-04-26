eaa3882e5bb4653d27f7a97ddbdcb37ed735831f
onkar <onksgk@gmail.com>
2026-04-26 22:46:51 +0530

    error handling: handling unhandled routes

--- app.js
+++ app.js
@@ -25,4 +25,13 @@ app.use((req, res, next) => {
 app.use("/api/v1/tours", tourRoutes);
 app.use("/api/v1/users", userRoutes);
 
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    statusCode: 404,
+    message: `Can't find route ${req.originalUrl}`,
+  });
+  next();
+});
+
 module.exports = app;
