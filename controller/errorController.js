0208fea65b84446cda7e3f8a129cec398ef23149
onkar <onksgk@gmail.com>
2026-05-01 14:26:13 +0530

    error handling: dev vs prod

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,10 +1,40 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "fail";
+const errorHandlerDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
     statusCode: err.statusCode,
+    error: err,
+    stack: err.stack,
   });
+};
+
+const errorHandlerProd = (err, res) => {
+  //Operational, trusted errors: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+      statusCode: err.statusCode,
+    });
+    //Programming or other unknown errors
+  } else {
+    console.error("Error 💥", err);
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong!",
+      statusCode: 500,
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "fail";
+
+  if (process.env.NODE_ENV == "development") {
+    errorHandlerDev(err, res);
+  } else {
+    errorHandlerProd(err, res);
+  }
   next();
 };
