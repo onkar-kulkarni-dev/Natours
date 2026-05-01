e9bd474ce306869c16d301a6edee22a7ee6b73b1
onkar <onksgk@gmail.com>
2026-05-01 15:12:29 +0530

    handling invalid db ids

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require("../utils/appError");
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
 const errorHandlerDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -31,10 +38,12 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "fail";
 
-  if (process.env.NODE_ENV == "development") {
+  if (!process.env.NODE_ENV == "development") {
     errorHandlerDev(err, res);
   } else {
-    errorHandlerProd(err, res);
+    let error = JSON.parse(JSON.stringify(err));
+    if (error.name == "CastError") error = handleCastErrorDB(error);
+    errorHandlerProd(error, res);
   }
   next();
 };
