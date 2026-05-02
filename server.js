f14aae0b75d18384621536b02de7aa22854721dc
onkar <onksgk@gmail.com>
2026-05-02 22:17:53 +0530

    error outside express - unhandled rejections

--- server.js
+++ server.js
@@ -10,15 +10,20 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD,
 );
 
-mongoose
-  .connect(DB)
-  .then((con) => console.log("DB connection successful!"))
-  .catch((err) => console.log("DB connection failed", err));
-
-
+mongoose.connect(DB).then((con) => console.log("DB connection successful!"));
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
+});
+
+//global async promise rejection handler
+process.on("unhandledRejection", (err) => {
+  console.log(err, "unhandled rejection");
+  //first shutting down server without receiving any new request, all prev requests get processed
+  server.close(() => {
+    //exiting the process with exit code 1 which means something is broken and 0 means success
+    process.exit(1); //here we pass either 0 or 1
+  });
 });
