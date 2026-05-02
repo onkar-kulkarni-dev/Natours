0ace002b27ddc0dd707542049c0b9f5fcf7bf18c
onkar <onksgk@gmail.com>
2026-05-02 20:20:22 +0530

    error handling: mongoose validation error

--- controller/errorController.js
+++ controller/errorController.js
@@ -12,6 +12,12 @@ const handleDuplicateRecordsDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleValidationErrorsDB = (err) => {
+  const errors = Object.values(err?.errors).map((el) => el.message);
+  const message = `Invalid data field value. ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
+
 const errorHandlerDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -45,12 +51,15 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "fail";
 
-  if (!process.env.NODE_ENV == "development") {
+  if (process.env.NODE_ENV == "development") {
     errorHandlerDev(err, res);
   } else {
     let error = JSON.parse(JSON.stringify(err));
-    if (error.name == "CastError") error = handleCastErrorDB(error);
-    if (error.errorResponse.code === 11000) error = handleDuplicateRecordsDB(error);
+    if (error?.name == "CastError") error = handleCastErrorDB(error);
+    if (error?.errorResponse?.code === 11000)
+      error = handleDuplicateRecordsDB(error);
+    if (error?.name == "ValidationError")
+      error = handleValidationErrorsDB(error);
     errorHandlerProd(error, res);
   }
   next();
