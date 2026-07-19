4ba01deb59bcd523e6294c4371f0263f3e2a5465
onkar <onksgk@gmail.com>
2026-07-19 21:57:21 +0530

    jwt token

--- controller/authController.js
+++ controller/authController.js
@@ -1,11 +1,22 @@
+const jwt = require('jsonwebtoken');
 const Users = require('../models/userModel');
 const catchAsync = require("../utils/catchAsync");
 
 exports.signUp = catchAsync(async (req, res, next) => {
-    const newUser = await Users.create(req.body);
+    const newUser = await Users.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+    });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_TOKEN_EXPIRES_IN
+    })
 
     res.status(201).json({
         status: "success",
+        token,
         data: {
             user: newUser
         }
