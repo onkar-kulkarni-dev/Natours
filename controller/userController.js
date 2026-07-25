87fa7c6e3cc5f41c5bcd7485a2474fa6df95ac78
onkar <onksgk@gmail.com>
2026-07-25 21:39:30 +0530

    update profile

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,14 @@
 const Users = require('../models/userModel');
+const AppError = require('../utils/appError');
 const catchAsync = require("../utils/catchAsync");
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {}
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el]
+  })
+  return newObj;
+}
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await Users.find();
@@ -11,6 +20,23 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     }
   })
 });
+
+exports.updateProfile = catchAsync(async (req, res, next) => {
+  if (req.body.password || req.body.confirmPassword) {
+    return next(new AppError('To update password use path: /updatePassword', 401))
+  }
+  const filteredFields = filterObj(req.body, 'name', 'email')
+  const updatedUser = await Users.findByIdAndUpdate(req.user._id, filteredFields, {
+    new: true,
+    runValidators: true
+  })
+  res.status(200).json({
+    status: "success",
+    data: {
+      updatedUser
+    }
+  })
+})
 
 exports.getUser = (req, res) => {
   res.status(500).json({
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,6 +12,8 @@ router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 router.patch('/updatePassword', authController.protect, authController.updatePassword)
 
+router.patch('/updateProfile', authController.protect, userController.updateProfile)
+
 router
   .route("/")
   .get(userController.getAllUsers)
