51bcd1ae7fa2b2a8b49cdf4ec8046c36495a0352
onkar <onksgk@gmail.com>
2026-07-26 15:59:30 +0530

    preventing parameter pollution

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize')
+const hpp = require('hpp');
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require('./controller/errorController');
@@ -27,10 +28,15 @@ const limiter = rateLimit({
 
 app.use('/api', limiter)
 
-app.use(express.json({limit: '30kb'})); //for payload from client
+app.use(express.json({ limit: '30kb' })); //for payload from client
 
 //data sanitization against NoSQL injection
 app.use(mongoSanitize());
+
+//prevent parameter pollution
+app.use(hpp({
+  whitelist: ['duration', 'price', 'difficulty', "ratingsAverage", "ratingsQuantity", "maxGroupSize"]
+}))
 
 //custom middleware
 
